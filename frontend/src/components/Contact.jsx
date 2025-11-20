0d299c8357f0590cdcce0f665f461b2fd3d4a607
shreyashkashyapanand01 <shreyashkashyapanand01@gmail.com>
2025-11-20 22:40:51 +0530

    fixed api endpoint

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -19,15 +19,15 @@ const Contact = () => {
         setStatus('loading');
 
         try {
-            const response = await fetch('https://shreyash-portfolio-backend.onrender.com', 
-           // const response = await fetch('http://localhost:5000/api/contact', 
+            const response = await fetch('https://shreyash-portfolio-backend.onrender.com/api/contact',
+                // const response = await fetch('http://localhost:5000/api/contact', 
                 {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formData),
+                });
 
             if (response.ok) {
                 setStatus('success');
